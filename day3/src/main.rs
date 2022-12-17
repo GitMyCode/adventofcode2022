81cece8db77670aa8fd60cd6a56cf3d2795ea4ee
martin.bouchard <martin.bouchard@unity3d.com>
2022-12-16 23:14:53 -0500

    day3-part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -29,6 +29,33 @@ Uppercase item types A through Z have priorities 27 through 52.
 In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
 
 Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+
+
+--- Part Two ---
+As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
+
 */
 use std::{fs::File, io::Read};
 
@@ -58,6 +85,16 @@ fn get_rucksack_compartment(line: &str) -> (&str, &str) {
     return splitted;
 }
 
+fn get_shared_value2(rufpack: (&str, &str, &str)) -> Option<char> {
+    for char in rufpack.0.chars() {
+        if (rufpack.1.contains(char) && rufpack.2.contains(char)) {
+            return Some(char);
+        }
+    }
+
+    return None;
+}
+
 fn get_share_values(compartments: (&str, &str)) -> Option<char> {
     let left = compartments.0;
     let right = compartments.1;
@@ -75,8 +112,24 @@ fn get_share_values(compartments: (&str, &str)) -> Option<char> {
     //    return shared;
 }
 
-fn main() {
-    let input = read_input_file();
+fn part2(input: String) {
+    let lines = input.lines();
+
+    let mut line_iter = lines.into_iter().peekable();
+
+    let mut all_shared:Vec<char> = Vec::new();
+    while line_iter.peek().is_some() {
+        let chunk: Vec<_> = line_iter.by_ref().take(3).collect();
+        let shared = get_shared_value2((chunk[0], chunk[1], chunk[2]));
+        all_shared.push(shared.unwrap());
+        println!("{:?}", chunk);
+    }
+    let result: i32 = all_shared.iter().map(|x| get_priority(x)).sum();
+
+    println!("{}", result);
+}
+
+fn part1(input: String) {
     let lines = input.lines();
     let mut all_shared: Vec<char> = Vec::new();
 
@@ -88,4 +141,9 @@ fn main() {
 
     let result: i32 = all_shared.iter().map(|x| get_priority(x)).sum();
     println!("{}", result);
+}
+
+fn main() {
+    let input = read_input_file();
+    part2(input);
 }
