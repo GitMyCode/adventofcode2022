bb0e9f4851b82f628403466da95b97225c037363
martin.bouchard <martin.bouchard@unity3d.com>
2022-12-16 22:47:25 -0500

    day3- part1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -30,8 +30,62 @@ In the above example, the priority of the item type that appears in both compart
 
 Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
 */
+use std::{fs::File, io::Read};
 
+fn read_input_file() -> String {
+    let mut file = File::open("input.txt").unwrap();
+    let mut fileContent = String::new();
+    file.read_to_string(&mut fileContent);
+    return fileContent;
+}
+
+static ALPHABET_VEC: [&'static char; 52] = [
+    &'a', &'b', &'c', &'d', &'e', &'f', &'g', &'h', &'i', &'j', &'k', &'l', &'m', &'n', &'o', &'p',
+    &'q', &'r', &'s', &'t', &'u', &'v', &'w', &'x', &'y', &'z', &'A', &'B', &'C', &'D', &'E', &'F',
+    &'G', &'H', &'I', &'J', &'K', &'L', &'M', &'N', &'O', &'P', &'Q', &'R', &'S', &'T', &'U', &'V',
+    &'W', &'X', &'Y', &'Z',
+];
+
+fn get_priority(&letter: &char) -> i32 {
+    let index = ALPHABET_VEC.iter().position(|&r| r == &letter).unwrap();
+    return index as i32 + 1;
+}
+
+const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
+
+fn get_rucksack_compartment(line: &str) -> (&str, &str) {
+    let splitted = line.split_at(line.len() / 2);
+    return splitted;
+}
+
+fn get_share_values(compartments: (&str, &str)) -> Option<char> {
+    let left = compartments.0;
+    let right = compartments.1;
+    let mut shared: Vec<char> = Vec::new();
+
+    for c in left.chars() {
+        if (right.contains(c)) {
+            return Some(c);
+            //shared.push(c);
+        }
+    }
+
+    return None;
+
+    //    return shared;
+}
 
 fn main() {
-    println!("Hello, world!");
+    let input = read_input_file();
+    let lines = input.lines();
+    let mut all_shared: Vec<char> = Vec::new();
+
+    for line in lines {
+        let compartments = get_rucksack_compartment(&line);
+        let shared = get_share_values(compartments);
+        all_shared.push(shared.unwrap());
+    }
+
+    let result: i32 = all_shared.iter().map(|x| get_priority(x)).sum();
+    println!("{}", result);
 }
